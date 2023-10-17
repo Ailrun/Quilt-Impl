46d0cbba4c96c30d54a32d3fd6667842cf67af61
Junyoung/"Clare" Jang <jjc9310@gmail.com>
2023-10-17 04:49:25 -0400

    Update interpreter

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,17 @@
-{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Hashable        (Hashable)
-import Data.Text.IO         qualified as T
-import Elevator.Evaluator   (eval)
-import Elevator.ModeSpec    (ElModeSpec (..))
-import Elevator.Parser      (fullParse)
-import Elevator.Syntax      (ElProgram (..), ElTop (..))
-import Elevator.TypeChecker (typeCheckProgIncremental, typeInfer)
-import GHC.Generics         (Generic)
-import System.IO            (hFlush, stdout)
-import Elevator.PrettyPrinter (prettyTerm, prettyType, prettyMode)
+import Data.Hashable          (Hashable)
+import Data.Text.IO           qualified as T
+import Elevator.Evaluator     (eval)
+import Elevator.ModeSpec      (ElModeSpec (..))
+import Elevator.Parser        (fullParse)
+import Elevator.PrettyPrinter (prettyMode, prettyTerm, prettyType)
+import Elevator.Syntax        (ElProgram (..), ElTop (..))
+import Elevator.TypeChecker   (typeCheckProgIncremental, typeInfer)
+import GHC.Generics           (Generic)
+import System.IO              (hFlush, stdout)
 
 data TwoMode = MCode | MProg
   deriving (Eq, Show, Generic, Hashable)
@@ -37,27 +38,34 @@ mainLoop prog@(ElProgram tops) n = do
   putStr "> "
   hFlush stdout
   l <- T.getLine
-  case fullParse ("<line " <> show n <> ">") l of
-    Right (Left (top@(ElDef x _ _ _) :: ElTop TwoMode)) ->
-      case typeCheckProgIncremental prog top of
-        Right () -> do
-          putStrLn $ "Top-level definition " <> show x <> " is defined"
-          mainLoop (ElProgram $ tops <> [top]) (n + 1)
+  if l == "@"
+  then do
+    l' <- T.getContents
+    mainFun l'
+  else mainFun l
+  where
+    mainFun str =
+      case fullParse ("<line " <> show n <> ">") str of
+        Right (Left (top@(ElDef x _ _ _) :: ElTop TwoMode)) ->
+          case typeCheckProgIncremental prog top of
+            Right () -> do
+              putStrLn $ "Top-level definition " <> show x <> " is defined"
+              mainLoop (ElProgram $ tops <> [top]) (n + 1)
+            Left err -> do
+              putStrLn $ "Error " <> "<line " <> show n <> " : " <> err
+              mainLoop prog (n + 1)
+        Right (Right tm) -> do
+          case typeInfer prog MProg tm of
+            Right ty -> do
+              case eval prog MProg tm of
+                Right r -> do
+                  putStrLn $ show (prettyTerm 0 r) <> " : " <> show (prettyMode MProg) <> " " <> show (prettyType 0 ty)
+                Left err -> putStrLn $ "Error " <> "<line " <> show n <> " : " <> err
+            Left err -> putStrLn $ "Error " <> "<line " <> show n <> " : " <> err
+          mainLoop prog (n + 1)
         Left err -> do
           putStrLn $ "Error " <> "<line " <> show n <> " : " <> err
           mainLoop prog (n + 1)
-    Right (Right tm) -> do
-      case typeInfer prog MProg tm of
-        Right ty -> do
-          case eval prog MProg tm of
-            Right r -> do
-              putStrLn $ show (prettyTerm 0 r) <> " : " <> show (prettyMode MProg) <> " " <> show (prettyType 0 ty)
-            Left err -> putStrLn $ "Error " <> "<line " <> show n <> " : " <> err
-        Left err -> putStrLn $ "Error " <> "<line " <> show n <> " : " <> err
-      mainLoop prog (n + 1)
-    Left err -> do
-      putStrLn $ "Error " <> "<line " <> show n <> " : " <> err
-      mainLoop prog (n + 1)
 
 main :: IO ()
 main = mainLoop (ElProgram []) 0
